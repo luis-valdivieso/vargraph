af56bd7ffe2bfadff961a57af792bb6ea9bbfe48
Luis Valdivieso <114484527+luis-valdivieso@users.noreply.github.com>
2026-08-18 14:34:53 +0200

    Added add_node and add_edge functions

--- src/vargraph/core.py
+++ src/vargraph/core.py
@@ -3,5 +3,20 @@ class VarGraph:
         self.directed = directed
         self.graph = {}
 
-    def add_edge(self, u, v, weight):
-        pass
+    def add_node(self, node):
+        """Adds an isolated node if it doesn't exist previously"""
+        if node not in self.graph:
+            self.graph[node] = {}
+
+    def add_edge(self, u, v, weight=1):
+        """Adds an edge between u and v with a weight"""
+        # If the nodes are new, we initialize the internal dictionaries
+        self.add_node(u)
+        self.add_node(v)
+            
+        # Create u-v connection
+        self.graph[u][v] = weight
+        
+        # If the graph is not directed, we create the v-u connection
+        if not self.directed:
+            self.graph[v][u] = weight
