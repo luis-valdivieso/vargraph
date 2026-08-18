17d50c51e80ac625075e2f74b5ddd7d85eb4ca74
Luis Valdivieso <114484527+luis-valdivieso@users.noreply.github.com>
2026-08-18 13:04:14 +0200

    Created initial code in src

---
+++ src/vargraph/__init__.py
@@ -0,0 +1,2 @@
+# Let the user do: from vargraph import VarGraph
+from .core import VarGraph
---
+++ src/vargraph/core.py
@@ -0,0 +1,6 @@
+class VarGraph:
+    def __init__(self, directed=False):
+        pass
+        
+    def add_edge(self, u, v, weight):
+        pass
