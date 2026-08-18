c51af51da41dcb664b525e5b3c227014957d0208
Luis Valdivieso <114484527+luis-valdivieso@users.noreply.github.com>
2026-08-18 14:32:51 +0200

    Created initial structure to store the graph

--- src/vargraph/core.py
+++ src/vargraph/core.py
@@ -1,6 +1,7 @@
 class VarGraph:
     def __init__(self, directed=False):
-        pass
-        
+        self.directed = directed
+        self.graph = {}
+
     def add_edge(self, u, v, weight):
         pass
