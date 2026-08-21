b0ea3a5ef41144dcc6d67d1f43fca8d2a5837579
Luis Valdivieso <114484527+luis-valdivieso@users.noreply.github.com>
2026-08-21 12:20:16 +0200

    Add sympy weights and free symbols getter

--- src/vargraph/core.py
+++ src/vargraph/core.py
@@ -1,7 +1,19 @@
+import sympy as sp
+
 class VarGraph:
     def __init__(self, directed=False):
         self.directed = directed
         self.graph = {}
+
+    @property
+    def free_symbols(self):
+        "Returns the complete set of free_symbols of the weights expressions of the graph"
+        symbols = set()
+        for neighbors in self.graph.values():
+            for weight in neighbors.values():
+                symbols.update(weight.free_symbols)
+                
+        return symbols
 
     def add_node(self, node):
         """Adds an isolated node if it doesn't exist previously"""
@@ -13,10 +25,11 @@ class VarGraph:
         # If the nodes are new, we initialize the internal dictionaries
         self.add_node(u)
         self.add_node(v)
-            
-        # Create u-v connection
-        self.graph[u][v] = weight
+
+        sympified_weight = sp.sympify(weight)
+        # Create u-v connection with sympy expression
+        self.graph[u][v] = sympified_weight
         
         # If the graph is not directed, we create the v-u connection
         if not self.directed:
-            self.graph[v][u] = weight
+            self.graph[v][u] = sympified_weight
